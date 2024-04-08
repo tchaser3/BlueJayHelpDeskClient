d4a568cd630768f93a15cfa1dba578d59085d661
Terry Holmes <tchaser3@outlook.com>
2024-04-08 05:50:25 -0400

    Updated .net

--- BlueJayHelpDesk/MainWindow.xaml.cs
+++ BlueJayHelpDesk/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ using HelpDeskDLL;
 using PhonesDLL;
 using System.Windows.Media.Converters;
 using System.Security.Cryptography;
+using System.IO;
+using System.Text;
 
 namespace BlueJayHelpDesk
 {
@@ -334,12 +336,78 @@ namespace BlueJayHelpDesk
 
                 TheMessagesClass.InformationMessage("Help Desk Ticket Number " + Convert.ToString(gintTicketID) + " Has Been Created");
 
+                const string message = "Would You Like to send a Document or Attach a File?";
+                const string caption = "Please Answer";
+                MessageBoxResult result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    AttachDocuments();
+                }
+
                 this.Close();
                 
             }
             catch(Exception Ex)
             {
                 TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Blue Jay Help Desk // Main Window // Submit Button " + Ex.Message);
+
+                TheMessagesClass.ErrorMessage(Ex.ToString());
+            }
+        }
+        private void AttachDocuments()
+        {
+            //setting local variables
+            DateTime datTransactionDate = DateTime.Now;
+            string strDocumentPath = "";
+            long intResult;
+            string strNewLocation = "";
+            string strTransactionName;
+            bool blnFatalError;
+            string strFileExtension;
+
+            try
+            {
+                Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+                dlg.FileName = "Document"; // Default file name                
+
+                // Show open file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+
+                // Process open file dialog box results
+                if (result == true)
+                {
+                    // Open document
+                    strDocumentPath = dlg.FileName.ToUpper();
+                }
+                else
+                {
+                    return;
+                }
+
+                FileInfo FileName = new FileInfo(strDocumentPath);
+
+                strFileExtension = FileName.Extension;
+
+                datTransactionDate = DateTime.Now;
+
+                intResult = datTransactionDate.Year * 10000000000 + datTransactionDate.Month * 100000000 + datTransactionDate.Day * 1000000 + datTransactionDate.Hour * 10000 + datTransactionDate.Minute * 100 + datTransactionDate.Second;
+                strTransactionName = Convert.ToString(intResult);
+
+                strNewLocation = "\\\\bjc\\shares\\Documents\\WAREHOUSE\\WhseTrac\\HelpDeskDocuments\\" + strTransactionName + strFileExtension;
+                
+                System.IO.File.Copy(strDocumentPath, strNewLocation);
+
+                blnFatalError = TheHelpDeskClass.InsertHelpDeskTicketDocumentation(gintTicketID, datTransactionDate, strNewLocation);
+
+                if (blnFatalError == true)
+                    throw new Exception();
+
+                TheMessagesClass.InformationMessage("The Document Has Been Saved");
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Help Desk Class // Main Window // Attach Documents " + Ex.Message);
 
                 TheMessagesClass.ErrorMessage(Ex.ToString());
             }
